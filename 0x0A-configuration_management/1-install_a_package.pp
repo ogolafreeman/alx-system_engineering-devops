dc12b0cd7e0bc8afe4416bdb184dd2287c15de90
Jacktone Okwemba <110520342+ogolafreeman@users.noreply.github.com>
2024-04-20 09:15:06 +0300

    install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# install flask using pip
+# installing flask using pip3
 
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.0.3',
   path    => ['/usr/bin'],
-  # require => Package['python3-pip'],
 }
