8a00905f3e510358fc455d948cc2b7c2130dfd8b
ogolafreeman <jacktoneokwemba14@gmail.com>
2024-06-04 22:42:35 +0300

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ from requests import get
 def number_of_subscribers(subreddit):
     """
     function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
+    for a given subreddit.
     """
 
     if subreddit is None or not isinstance(subreddit, str):
